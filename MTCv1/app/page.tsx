ea8baac4956a873b124e2494d283b4260215ce29
Apps4u <film4sports@gmail.com>
2026-01-25 11:26:51 -0500

    Add files via upload

---
+++ MTCv1/app/page.tsx
@@ -0,0 +1,18 @@
+'use client'
+
+export default function LandingPage() {
+  return (
+    <iframe 
+      src="/landing.html" 
+      title="Mono Tennis Club"
+      style={{
+        position: 'fixed',
+        top: 0,
+        left: 0,
+        width: '100vw',
+        height: '100vh',
+        border: 'none',
+      }}
+    />
+  )
+}
