aaed5286fc83ee81d40360686a47edbd5d2abdad
Apps4u <film4sports@gmail.com>
2026-01-25 19:59:25 -0500

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,7 +4,22 @@ module.exports = {
     './app/**/*.{js,ts,jsx,tsx,mdx}',
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        'mono-green': {
+          50: '#f4f7ed',
+          100: '#e6ecd8',
+          200: '#ced9b5',
+          300: '#afc088',
+          400: '#91a662',
+          500: '#6b7a3d',
+          600: '#576432',
+          700: '#434d28',
+          800: '#383f24',
+          900: '#313723',
+        }
+      }
+    },
   },
   plugins: [],
 }
