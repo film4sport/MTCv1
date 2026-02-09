61b9ef591b05c62e1c303e0d81f91d14e8184893
Your Name <film4sports@gmail.com>
2026-02-09 00:08:29 -0500

    changes

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,7 +4,23 @@ module.exports = {
     './app/**/*.{js,ts,jsx,tsx,mdx}',
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        'mono-green': {
+          50: '#f7faf0',
+          100: '#ecf4dd',
+          200: '#dceabf',
+          300: '#c4db96',
+          400: '#a8c76b',
+          500: '#8bb44a',
+          600: '#6b9a36',
+          700: '#547a2c',
+          800: '#456128',
+          900: '#3b5225',
+        }
+      }
+    },
   },
   plugins: [],
 }
+
